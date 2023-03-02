d665cd7478a4427fbe92c5d8b8760ef06c4f89ec
JeanetteSkaarup <37311867+JeanetteSkaarup@users.noreply.github.com>
2023-03-02 11:20:34 +0100

    Create index.php

---
+++ week2/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Jeanette Skaarup</title>
+</head>
+<body>
+	<h1>Welcome to Autos Database</h1>
+<div class="container">
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to 
+<a href="autos.php">autos.php</a> without logging in - it should fail with an error message.
+</p>
+</div>
+</body>
+</html>
